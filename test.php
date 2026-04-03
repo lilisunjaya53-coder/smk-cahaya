51c1fb97707174184d9b68349bf773b667248a28
anggara <anggara.bw@gmail.com>
2026-04-03 18:00:56 +0700

    feat: changed test.php file

--- test.php
+++ test.php
@@ -1,10 +1,9 @@
 <?php
 // generate_hash.php
-$password_plain = '@Admin123'; // Tentukan password Kepsek di sini
+$password_plain = 'LiliSunjaya'; // Tentukan password Kepsek di sini
 $hashed_password = password_hash($password_plain, PASSWORD_DEFAULT);
 
 echo "Password Plaintext: " . $password_plain . "\n";
 echo "Hash BCrypt (Siap Disalin): **" . $hashed_password . "**\n";
 echo "================================================================================\n";
 echo "SALIN SELURUH STRING DI ATAS (TERMASUK \$2y\$...) DAN UPDATE DI TABEL USERS VIA PHPMYADMIN.\n";
-?>
